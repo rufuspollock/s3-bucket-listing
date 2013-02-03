af1fa082cadff617c690d752e64c893a7950a661
Rufus Pollock <rufus.pollock@okfn.org>
2013-02-03 23:48:27 +0000

    [all][m]: working first-pass implementation.

---
+++ README.md
@@ -0,0 +1,4 @@
+Create nice directory listings for s3 buckets using only javascript and HTML.
+
+Inspiration from http://aws.amazon.com/code/Amazon-S3/1713
+
---
+++ list.js
@@ -0,0 +1,41 @@
+var url = location.protocol + '//' + location.hostname;
+$.get(url)
+  .done(function(data) {
+    var xml = $(data);
+    var files = $.map(xml.find('Contents'), function(item) {
+      item = $(item);
+      return {
+        Key: item.find('Key').text(),
+        LastModified: item.find('LastModified').text(),
+        Size: item.find('Size').text(),
+      }
+    });
+    renderTable(files);
+  });
+
+function renderTable(files) {
+  var cols = [ 45, 30, 15 ];
+  var content = padRight('Key', cols[0]) + '  ' + padRight('Last Modified', cols[1]) + '  ' + padRight('Size', cols[2]) + '\n';
+  content += new Array(cols[0] + cols[1] + cols[2] + 4).join('-') + '\n';
+  $.each(files, function(idx, item) {
+    var key = item.Key;
+    var row = '<a href="' + key + '">' + padRight(item.Key, cols[0]) + '</a>  ';
+    row += padRight(item.LastModified, cols[1]) + '  ';
+    row += padRight(item.Size, cols[2]);
+    content += row + '\n';
+  });
+
+  document.getElementById('listing').innerHTML = '<pre>' + content + '</pre>';
+}
+
+function padRight(padString, length) {
+  var str = padString.slice(0, length-3);
+  if (padString.length > str.length) {
+    str += '...';
+  }
+  while (str.length < length) {
+    str = str + ' ';
+  }
+  return str;
+}
+
