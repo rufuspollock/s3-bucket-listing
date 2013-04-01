108388071fab0bfd3fe3bf66f6d1d0501d67843c
Steve Shirkey <steveshirkey@gmail.com>
2013-04-02 00:31:15 +0700

    added success/error dump to index

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,11 @@
 'use strict';
 
 /* Controllers */
-function ListController($scope,$http) {
+function ListController($scope,$http,$window) {
     $scope.bucket = {};
-    $scope.bucket.url = 'http://files.variaprima.com.s3-ap-southeast-1.amazonaws.com';
+    //'http://data.openspending.org.s3-eu-west-1.amazonaws.com';
+    $scope.bucket.url = $window.location.protocol=='file:' ? 'http://files.variaprima.com.s3-ap-southeast-1.amazonaws.com' :
+        $window.location.protocol + '//' + $window.location.hostname.replace('s3-website','s3');
     getBucketList($scope, $http);
     $scope.updateBucketUrl = function($scope, $http){
         console.log('Bucket URL changed: ' + $scope.bucket.url)
@@ -11,12 +13,20 @@ function ListController($scope,$http) {
     };
     //scope.bucket = { url: 'http://data.openspending.org.s3-eu-west-1.amazonaws.com' };
 }
-ListController.$inject = ['$scope','$http'];
+ListController.$inject = ['$scope','$http','$window'];
 
 function getBucketList($scope, $http){
+    $scope.success=$scope.error='';
     $http.defaults.useXDomain = true;
     $http.get($scope.bucket.url).
-        success(function(data) {
+        success(function(data, status, headers, config) {
+            var success = {
+                status: status,
+                headers: headers,
+                config: config
+            };
+            $scope.success=success;
+            console.log(success);
             var xml = angular.element(data);
             var s3contents = _.filter(xml.children(),function(x) { return x.tagName=='CONTENTS' });
             var contents = _.chain(s3contents).
@@ -34,8 +44,13 @@ function getBucketList($scope, $http){
             files = _.filter(angular.element(files),function(item){ return item.key.indexOf("app/") != 0;});
             $scope.bucket.files = files;
         }).
-        error(function(error) {
-            alert('There was an error');
+        error(function(data, status, headers, config) {
+            var error = {
+                status: status,
+                headers: headers,
+                config: config
+            };
+            $scope.error = error;
             console.log(error);
         });
 }
