c93057522103df8d99c3d1c41c37329a80b9cf97
Steve Shirkey <steveshirkey@gmail.com>
2013-04-01 23:42:58 +0700

    new formatting

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,25 @@
 'use strict';
 
 /* Controllers */
-function ListController(scope,http) {
-    scope.bucket = { url: 'http://files.variaprima.com.s3-ap-southeast-1.amazonaws.com' };
-    http.defaults.useXDomain = true;
-    http.get(scope.bucket.url)
-        .success(function(data) {
+function ListController($scope,$http) {
+    $scope.bucket = {};
+    $scope.bucket.url = 'http://files.variaprima.com.s3-ap-southeast-1.amazonaws.com';
+    getBucketList($scope, $http);
+    $scope.updateBucketUrl = function($scope, $http){
+        console.log('Bucket URL changed: ' + $scope.bucket.url)
+        getBucketList($scope, $http);
+    };
+    //scope.bucket = { url: 'http://data.openspending.org.s3-eu-west-1.amazonaws.com' };
+}
+ListController.$inject = ['$scope','$http'];
+
+function getBucketList($scope, $http){
+    $http.defaults.useXDomain = true;
+    $http.get($scope.bucket.url).
+        success(function(data) {
             var xml = angular.element(data);
             var s3contents = _.filter(xml.children(),function(x) { return x.tagName=='CONTENTS' });
-            scope.bucket.contents = _.chain(s3contents).
+            var contents = _.chain(s3contents).
                 map(function(item) {
                     item = angular.element(item);
                     return {
@@ -19,12 +30,12 @@ function ListController(scope,http) {
                     };
                 }).
                 value();
-            scope.bucket.files = _.filter(angular.element(scope.bucket.contents),function(item){ return item.size > 0;});
-            scope.bucket.files = _.filter(angular.element(scope.bucket.files),function(item){ return item.key.indexOf("app/") != 0;});
-        })
-        .error(function(error) {
+            var files = _.filter(angular.element(contents),function(item){ return item.size > 0;});
+            files = _.filter(angular.element(files),function(item){ return item.key.indexOf("app/") != 0;});
+            $scope.bucket.files = files;
+        }).
+        error(function(error) {
             alert('There was an error');
             console.log(error);
         });
 }
-ListController.$inject = ['$scope','$http'];
--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,20 @@ angular.module('bucketListApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).
+  directive('ngModelOnblur', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, elm, attr, ngModelCtrl) {
+            if (attr.type === 'radio' || attr.type === 'checkbox') return;
+
+            elm.unbind('input').unbind('keydown').unbind('change');
+            elm.bind('blur', function() {
+                scope.$apply(function() {
+                    ngModelCtrl.$setViewValue(elm.val());
+                });
+            });
+        }
+    };
+});
