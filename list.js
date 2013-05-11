e7813a3f792e5ff3dfe3f07ba9c899ee31a6aaef
Rufus Pollock <rufus.pollock@okfn.org>
2013-05-11 12:16:36 +0100

    [#3][m]: only show current directory - fixes #3 (thx to @alzadude).
    * Also minor refactor to move out templating to a separate function.
    * Taken from this patch - https://github.com/nineworthies/s3-bucket-listing/commit/38fa104cccecfee4c6796cc5e2262f8cc5d60681

--- list.js
+++ list.js
@@ -1,8 +1,8 @@
 jQuery(function($) {
   if (typeof BUCKET_URL != 'undefined') {
-    var url = BUCKET_URL;
+    var url = BUCKET_URL + location.search;
   } else {
-    var url = location.protocol + '//' + location.hostname;
+    var url = location.href;
   }
   $.get(url)
     .done(function(data) {
@@ -15,7 +15,7 @@ jQuery(function($) {
           Size: item.find('Size').text(),
         }
       });
-      renderTable(files);
+      renderTable(files, xml.find('Prefix').text());
     })
     .fail(function(error) {
       alert('There was an error');
@@ -23,20 +23,42 @@ jQuery(function($) {
     });
 });
 
-function renderTable(files) {
+function renderTable(files, prefix) {
   var cols = [ 45, 30, 15 ];
-  var content = padRight('Last Modified', cols[1]) + '  ' + padRight('Size', cols[2]) + 'Key \n';
-  content += new Array(cols[0] + cols[1] + cols[2] + 4).join('-') + '\n';
+  var content = [];
+  content.push(padRight('Last Modified', cols[1]) + '  ' + padRight('Size', cols[2]) + 'Key \n');
+  content.push(new Array(cols[0] + cols[1] + cols[2] + 4).join('-') + '\n');
+  
+  if (prefix) {
+    files.shift();
+    var item = {
+      Key: prefix.replace(/\/$/, '').split('/').slice(0, -1).concat('').join('/'),
+      LastModified: '',
+      Size: ''
+    };
+    renderRow(item, cols, '../', content);
+  }
+  
   $.each(files, function(idx, item) {
-    var key = item.Key;
-    var row = '';
-    row += padRight(item.LastModified, cols[1]) + '  ';
-    row += padRight(item.Size, cols[2]);
-    row += '<a href="' + key + '">' + item.Key + '</a>';
-    content += row + '\n';
+    var suffix = item.Key.substring(prefix.length);
+    var slashCount = (suffix.split('/').length - 1);
+    if (slashCount == 0 || (slashCount == 1 && suffix.indexOf('/') == suffix.length - 1)) {
+      renderRow(item, cols, suffix, content);
+    }
   });
 
-  document.getElementById('listing').innerHTML = '<pre>' + content + '</pre>';
+  document.getElementById('listing').innerHTML = '<pre>' + content.join('') + '</pre>';
+}
+
+function renderRow(item, cols, keyText, content) {
+  var key = item.Key;
+  var row = '';
+  row += padRight(item.LastModified, cols[1]) + '  ';
+  row += padRight(item.Size, cols[2]);
+  row += '<a href="'; 
+  row += key && key.indexOf('/') == -1 ? key : location.href.split('?')[0] + '?prefix=' + key;
+  row += '">' + keyText + '</a>';
+  content.push(row + '\n');
 }
 
 function padRight(padString, length) {
