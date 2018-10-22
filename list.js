8bd2ca082d9af90f69f8d48f9e65b17bd0637fcd
Andrew Marentis <amarentis@netroadshow.com>
2018-10-22 14:58:18 -0400

    Updating s3 url

--- list.js
+++ list.js
@@ -3,8 +3,8 @@ if (typeof AUTO_TITLE != 'undefined' && AUTO_TITLE == true) {
 }
 
 if (typeof S3_REGION != 'undefined') {
-  var BUCKET_URL = 'http://' + location.hostname + '.' + S3_REGION + '.amazonaws.com'; // e.g. just 's3' for us-east-1 region
-  var BUCKET_WEBSITE_URL = location.protocol + '//' + location.hostname;
+  var BUCKET_URL = 'https://' + BUCKET_NAME + '.' + S3_REGION + '.amazonaws.com/'; // e.g. just 's3' for us-east-1 region
+  var BUCKET_WEBSITE_URL = location.protocol + '//' + location.hostname + '/' + BUCKET_NAME;
 }
 
 if (typeof S3BL_IGNORE_PATH == 'undefined' || S3BL_IGNORE_PATH != true) {
