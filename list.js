93f71a422c6c1e5a9dfebb7d7973ecd2c4dc3a2f
Ivan Babrou <github@ivan.computer>
2021-02-25 16:55:33 -0800

    Simplify sorting by doing it inplace

--- list.js
+++ list.js
@@ -131,9 +131,7 @@ function getS3Data(marker, html) {
         // See url for example:
         // http://esp-link.s3-website-us-east-1.amazonaws.com/
         if (S3B_SORT != 'DEFAULT') {
-          var sortedFiles = info.files;
-          sortedFiles.sort(sortFunction);
-          info.files = sortedFiles;
+          info.files.sort(sortFunction);
         }
 
         buildNavigation(info);
